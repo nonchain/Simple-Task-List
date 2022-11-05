246ee13d71d724a945906711ebf6dbcc9e8ba3fe
progali007 <code.progali@gmail.com>
2022-11-06 01:12:51 +0330

    Setup some custom settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,28 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '390px',
+        md: '540px',
+        lg: '768px',
+        xl: '1024px',
+        '2xl': '1280px',
+        '3xl': '1440px',
+      },
+      colors: {
+        background: '#F8FFF8',
+        primary: '#7DCE13',
+        title: '#282D2B',
+        text: '#585D5B',
+        light: {
+          100: '#F5FCEB',
+          200: '#EAEAEA',
+          300: '#959C9B',
+          400: '#686D6B',
+        }
+      },
+    },
   },
   plugins: [],
 }
