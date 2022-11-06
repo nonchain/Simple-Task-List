ef7369c49d78438982dbaa255b1f6d197b23c1e3
progali007 <code.progali@gmail.com>
2022-11-06 04:44:34 +0330

    Change some styles and add TaskList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import TaskList from './components/Task/TaskList'
+import Card from './components/UI/Card'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='px-4 py-8 min-h-screen bg-background flex flex-col'>
+      <Card className='bg-[#eaf6f6]' title='All Data'>
+        <TaskList />
+      </Card>
+    </div>
   )
 }
 
